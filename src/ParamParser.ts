fe1c31e85f6d94568ea8fb07f9b722c891a6d7c0
yang <151276679@qq.com>
2024-05-26 16:20:30 +0800

    多点找色参数解析器

--- src/ParamParser.ts
+++ src/ParamParser.ts
@@ -5,26 +5,25 @@ interface ParamParser {
 
 class AjsParamParser implements ParamParser {
   public parseColorParam(colorPath: string):  [string, Array<[number, number, string]>, { region: OmniRegion, threshold: number }] {
-    let input = colorPath.replace(new RegExp('"', 'g'),'') //todo
     const indexes: number[] = []
     const regex = new RegExp(',', 'g')
     let match: RegExpExecArray | null
 
-    while ((match = regex.exec(input)) !== null) {
+    while ((match = regex.exec(colorPath)) !== null) {
       indexes.push(match.index);
     }
-    const kuohaoIndex =  input.indexOf('{')
-    const baseColor = input.substring(indexes[0]+1, indexes[1]).replace(new RegExp('"', 'g'),'')
-    const colors = JSON.parse(input.substring(indexes[1]+1, kuohaoIndex-1))
+    const kuohaoIndex =  colorPath.indexOf('{')
+    const baseColor = colorPath.substring(indexes[0]+1, indexes[1]).replace(new RegExp('"', 'g'),'')
+    const colors = JSON.parse(colorPath.substring(indexes[1]+1, kuohaoIndex-1))
 
-    const a = input.substring(kuohaoIndex)
+    const a = colorPath.substring(kuohaoIndex)
     // const options = JSON.parse(input.substring(kuohaoIndex))
     // const options = eval("(" + input.substring(kuohaoIndex) + ")");
 
     //将region ,threshold 用 "" 包裹起来，以便于解析成json
     const regex2 = /(\w+):/g;
     const options = JSON.parse(
-        input.substring(kuohaoIndex)
+        colorPath.substring(kuohaoIndex)
         .replace(regex2, '"$1":')
         .replace(/("threshold":\[)(\d+)(\])/g, '"threshold":'+'$2')
     );
@@ -35,6 +34,9 @@ class AjsParamParser implements ParamParser {
   }
 }
 
+/**
+ * 将综合图色助手 生成的 多点找色字符串解析成 autojs6的格式
+ */
 export function parseAjColorParam(colorPath: string):  [string, Array<[number, number, string]>, { region: OmniRegion, threshold: number }] {
   const parser = new AjsParamParser()
   return parser.parseColorParam(colorPath)
--- tests/colorParamParser.test.ts
+++ tests/colorParamParser.test.ts
@@ -1,14 +1,16 @@
-import {parseParam} from "../src/colorParamParser";
+import {parseAjColorParam} from "../src/ParamParser";
+import {colorConfig} from "../src/colorConfig";
 
-
+// "autojs/626/1208",
+// "#949ca9",
+// [[-37,-33,"#f26155"],[-28,-52,"#d74b41"],[36,-37,"#949ca9"],[72,16,"#78a5cc"],[2,-9,"#ace8f9"],[-2,50,"#d5d5d1"]],
+// {region:[576,1171,127,105],threshold:[26]}
 test('test parse color param', () => {
-  // const [mat, baseColor, colors, offset1, offset2, rect, similarity] = parse("params");
-  const params = parseParam("params");
+  const params = parseAjColorParam(colorConfig.mainWindow.mainCityColor);
   let p1 = params.at(0)
-
-  console.log("test")
-  expect(
-      true,
-  ).toBe(true)
+  expect(params).toBeInstanceOf(Array)
+  expect(params[0]).toEqual("#949ca9")
+  expect(params[1]).toEqual([[-37,-33,"#f26155"],[-28,-52,"#d74b41"],[36,-37,"#949ca9"],[72,16,"#78a5cc"],[2,-9,"#ace8f9"],[-2,50,"#d5d5d1"]])
+  expect(params[2]).toEqual({region: [576, 1171, 127, 105], threshold: 26})
 })
 
