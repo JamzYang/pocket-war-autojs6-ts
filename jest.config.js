eb952b900420195fa6e3a29a789ba19572ba2414
yang <151276679@qq.com>
2024-10-05 23:18:09 +0800

    提高jest转译效率

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   // roots: ['<rootDir>/tests'],
-  preset: 'ts-jest',
+  // preset: 'ts-jest',
   // testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts']
-
+  testMatch: ['**/tests/**/*.test.ts'],
+  transform: {
+    // 使用 swc 转译 JavaScript 和 TypeScrit
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+    // 静态资源 stub 转译
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+        "jest-transform-stub",
+  },
 };
